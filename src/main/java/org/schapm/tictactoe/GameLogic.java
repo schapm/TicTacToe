c24b4186600b8d164f5356c75f8c9b90f96fada2
schapm <ozuhouz@pm.me>
2022-03-23 13:24:07 +0000

    ADD: Check for winners

--- src/main/java/org/schapm/tictactoe/GameLogic.java
+++ src/main/java/org/schapm/tictactoe/GameLogic.java
@@ -2,6 +2,9 @@ package org.schapm.tictactoe;
 
 import javafx.scene.control.Button;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  *
  * @author schapm
@@ -38,6 +41,13 @@ public class GameLogic {
 
         button.setText(turn.getName());
 
+        if (checkForWin(buttonValuesToList())) {
+            // Current turn is winner
+            gameView.setWinner(turn);
+            isFinished = true;
+            return;
+        }
+
         // Check for stalemate
         if (areButtonsFilled()) {
             gameView.setDraw();
@@ -70,6 +80,59 @@ public class GameLogic {
         }
 
         gameView.setTurn(turn);
+    }
+
+    private boolean checkForWin(List<String> values) {
+        for (String string : values) {
+            if (string.trim().equals(player0.getName() + player0.getName() + player0.getName())) {
+                return true;
+            }
+
+            if (string.trim().equals(playerX.getName() + playerX.getName() + playerX.getName())) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    private List<String> buttonValuesToList() {
+        List<String> allValues = new ArrayList<>();
+
+        // Columns
+        int buttonIndex = 0;
+        for (int x = 1; x <= 3; x++) {
+            StringBuilder colSum = new StringBuilder();
+            for (int y = 1; y <= 3; y++) {
+                colSum.append(buttons[buttonIndex].getText());
+                buttonIndex++;
+            }
+
+            allValues.add(colSum.toString());
+        }
+
+        // Rows
+        String[] row = {"", "", ""};
+        int index = 0;
+        while (index <= row.length - 1) {
+            row[index] += String.valueOf(allValues.get(0).charAt(index)) + String.valueOf(allValues.get(1).charAt(index))
+                    + String.valueOf(allValues.get(2).charAt(index));
+            allValues.add(row[index]);
+            index++;
+        }
+
+        String[] diagonals = {"", ""};
+        // Left diagonal
+        diagonals[0] = String.valueOf(allValues.get(0).charAt(0)) + String.valueOf(allValues.get(1).charAt(1))
+                + String.valueOf(allValues.get(2).charAt(2));
+        allValues.add(diagonals[0]);
+
+        // Right diagonal
+        diagonals[1] = String.valueOf(allValues.get(0).charAt(2)) + String.valueOf(allValues.get(1).charAt(1))
+                + String.valueOf(allValues.get(2).charAt(0));
+        allValues.add(diagonals[1]);
+
+        return allValues;
     }
 
 }
